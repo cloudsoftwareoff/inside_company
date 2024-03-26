717169f83cdab36148311bcde5ae9326c0135ad0
cloudsoftwareoff <chahid.ahmadi2@gmail.com>
2024-03-26 17:18:46 +0100

    v0.1

---
+++ lib/views/auth/main_auth.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:inside_company/views/auth/login.dart';
+import 'package:inside_company/views/auth/signup.dart';
+
+class MainAuth extends StatefulWidget {
+  const MainAuth({super.key});
+
+  @override
+  State<MainAuth> createState() => _MainAuthState();
+}
+
+class _MainAuthState extends State<MainAuth> {
+  PageController controller = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 2,
+        controller: controller,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return LoginScreen(
+              controller: controller,
+            );
+          } else {
+            return SingUpScreen(
+              controller: controller,
+            );
+          }
+
+          // else {
+          //   return VerifyScreen(
+          //     controller: controller,
+          //   );
+          // }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/wrapper.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:inside_company/views/auth/main_auth.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          User? user = snapshot.data;
+
+          // Ensure the user is logged in
+          if (user != null) {
+            // Return home
+            return Text("");
+          } else {
+            // Show auth or login screen
+            return MainAuth();
+          }
+        } else {
+          // Show loading indicator or another placeholder widget
+          return CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
